b08e24950e6fc829b42aa3f66917ff6814b53b30
Brandon <d2sm10@hotmail.com>
2016-01-17 21:58:24 -0700

    Added requires

--- lib/bblib.rb
+++ lib/bblib.rb
@@ -4,7 +4,11 @@ require_relative "file/bbfile"
 require_relative "time/bbtime"
 require_relative "hash/bbhash"
 require_relative "math/bbmath"
+require_relative "object/bbobject"
+require_relative "array/bbarray"
 require 'fileutils'
+require 'net/http'
+require 'uri'
 
 module BBLib
 
--- lib/time/bbtime.rb
+++ lib/time/bbtime.rb
@@ -1,4 +1,4 @@
-
+require_relative 'task_timer'
 
 module BBLib
 
