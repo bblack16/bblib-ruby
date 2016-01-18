2413d32f9805d256ed5342ec983a9d79bc3523ef
Brandon <d2sm10@hotmail.com>
2016-01-17 21:59:42 -0700

    Created Object module and added obj_to_hash method.

---
+++ lib/object/bbobject.rb
@@ -0,0 +1,28 @@
+class Object
+
+  def obj_to_hash
+    return {self => nil} unless !self.instance_variables.empty?
+    hash = {}
+    self.instance_variables.each do |var|
+      value = self.instance_variable_get(var)
+      if value.is_a? Array
+        hash[var.to_s.delete("@")] = value.map{ |v| v.respond_to?(:obj_to_hash) && !v.instance_variables.empty? ? v.obj_to_hash : v }
+      elsif value.is_a? Hash
+        begin
+          if !hash[var.to_s.delete("@")].is_a?(Hash) then hash[var.to_s.delete("@")] = Hash.new end
+        rescue
+          hash[var.to_s.delete("@")] = Hash.new
+        end
+        value.each do |k, v|
+          hash[var.to_s.delete("@")][k.to_s.delete("@")] = v.respond_to?(:obj_to_hash) && !v.instance_variables.empty? ? v.obj_to_hash : v
+        end
+      elsif value.respond_to?(:obj_to_hash) && !value.instance_variables.empty?
+        hash[var.to_s.delete("@")] = value.obj_to_hash
+      else
+        hash[var.to_s.delete("@")] = value
+      end
+    end
+    return hash
+  end
+
+end
