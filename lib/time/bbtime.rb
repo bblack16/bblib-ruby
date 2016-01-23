7c8cb3a17b0471c3cb5adbcbd240849fc93671af
Brandon <d2sm10@hotmail.com>
2016-01-23 11:02:09 -0700

    Added support down to yoctoseconds. Changed milliseconds to be the x1 multiplier. This solves some rounding issues down to the microsecond level caused by floating point innacuracies.

--- lib/time/bbtime.rb
+++ lib/time/bbtime.rb
@@ -4,16 +4,16 @@ module BBLib
 
   # Parses known time based patterns out of a string to construct a numeric duration.
   def self.parse_duration str, output: :sec
-    secs = 0.0
+    msecs = 0.0
     TIME_EXPS.each do |k, v|
       v[:exp].each do |e|
         numbers = str.downcase.scan(/(?=\w|\D|\A)\d?\.?\d+[[:space:]]*#{e}(?=\W|\d|\z)/i)
         numbers.each do |n|
-          secs+= n.to_i * v[:mult]
+          msecs+= n.to_f * v[:mult]
         end
       end
     end
-    secs / (TIME_EXPS[output][:mult].to_f rescue 1)
+    msecs / (TIME_EXPS[output][:mult] rescue 1)
   end
 
   # Turns a numeric input into a time string.
@@ -24,47 +24,84 @@ module BBLib
     n, done = num * TIME_EXPS[input.to_sym][:mult], false
     TIME_EXPS.reverse.each do |k, v|
       next unless !done
-      div = n / v[:mult]
-      if div > 1
-        expression << "#{div.floor}#{v[:styles][style]}#{div.floor > 1 && style != :short ? "s" : nil}"
-        n-= div.floor * v[:mult]
-      end
       if k == stop then done = true end
+      div = n / v[:mult]
+      puts "#{k} = #{div} - #{v[:mult]}"
+      if div >= 1
+        val = (done ? div.round : div.floor)
+        expression << "#{val}#{v[:styles][style]}#{val > 1 && style != :short ? "s" : nil}"
+        n-= val.to_f * v[:mult]
+      end
     end
     expression.join ' '
   end
 
   TIME_EXPS = {
-    milli: {
+    yocto: {
+      mult: 0.000000000000000000001,
+      styles: {full: ' yoctosecond', medium: ' yocto', short: 'ys'},
+      exp: ['yoctosecond', 'yocto', 'yoctoseconds', 'yoctos', 'ys']
+    },
+    zepto: {
+      mult: 0.000000000000000001,
+      styles: {full: ' zeptosecond', medium: ' zepto', short: 'zs'},
+      exp: ['zeptosecond', 'zepto', 'zeptoseconds', 'zeptos', 'zs']
+    },
+    atto: {
+      mult: 0.000000000000001,
+      styles: {full: ' attosecond', medium: ' atto', short: 'as'},
+      exp: ['attoseconds', 'atto', 'attoseconds', 'attos', 'as']
+    },
+    femto: {
+      mult: 0.000000000001,
+      styles: {full: ' femtosecond', medium: ' fempto', short: 'fs'},
+      exp: ['femtosecond', 'fempto', 'femtoseconds', 'femptos', 'fs']
+    },
+    pico: {
+      mult: 0.000000001,
+      styles: {full: ' picosecond', medium: ' pico', short: 'ps'},
+      exp: ['picosecond', 'pico', 'picoseconds', 'picos', 'ps']
+    },
+    nano: {
+      mult: 0.000001,
+      styles: {full: ' nanosecond', medium: ' nano', short: 'ns'},
+      exp: ['nanosecond', 'nano', 'nanoseconds', 'nanos', 'ns']
+    },
+    micro: {
       mult: 0.001,
-      styles: {full: ' millisecond', medium: ' milli', short: 'ms'},
+      styles: {full: ' microsecond', medium: ' micro', short: 'μs'},
+      exp: ['microsecond', 'micro', 'microseconds', 'micros', 'μs']
+    },
+    milli: {
+      mult: 1,
+      styles: {full: ' millisecond', medium: ' mil', short: 'ms'},
       exp: ['ms', 'mil', 'mils', 'milli', 'millis', 'millisecond', 'milliseconds', 'milsec', 'milsecs', 'msec', 'msecs', 'msecond', 'mseconds']},
     sec: {
-      mult: 1,
+      mult: 1000,
       styles: {full: ' second', medium: ' sec', short: 's'},
       exp: ['s', 'sec', 'secs', 'second', 'seconds']},
     min: {
-      mult: 60,
+      mult: 60000,
       styles: {full: ' minute', medium: ' min', short: 'm'},
       exp: ['m', 'mn', 'mns', 'min', 'mins', 'minute', 'minutes']},
     hour: {
-      mult: 3600,
+      mult: 3600000,
       styles: {full: ' hour', medium: ' hr', short: 'h'},
       exp: ['h', 'hr', 'hrs', 'hour', 'hours']},
     day: {
-      mult: 86400,
+      mult: 86400000,
       styles: {full: ' day', medium: ' day', short: 'd'},
       exp: ['d', 'day' 'days']},
     week: {
-      mult: 604800,
+      mult: 604800000,
       styles: {full: ' week', medium: ' wk', short: 'w'},
       exp: ['w', 'wk', 'wks', 'week', 'weeks']},
     month: {
-      mult: 2592000,
+      mult: 2592000000,
       styles: {full: ' month', medium: ' mo', short: 'mo'},
       exp: ['mo', 'mon', 'mons', 'month', 'months', 'mnth', 'mnths', 'mth', 'mths']},
     year: {
-      mult: 31536000,
+      mult: 31536000000,
       styles: {full: ' year', medium: ' yr', short: 'y'},
       exp: ['y', 'yr', 'yrs', 'year', 'years']}
   }
