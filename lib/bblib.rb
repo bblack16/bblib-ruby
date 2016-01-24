97356dfb4c9b9b721756a9491c94924164394afb
Brandon <d2sm10@hotmail.com>
2016-01-24 09:01:13 -0700

    Added thread module and changed math to number.

--- lib/bblib.rb
+++ lib/bblib.rb
@@ -3,9 +3,10 @@ require_relative "string/bbstring"
 require_relative "file/bbfile"
 require_relative "time/bbtime"
 require_relative "hash/bbhash"
-require_relative "math/bbmath"
+require_relative "number/bbnumber"
 require_relative "object/bbobject"
 require_relative "array/bbarray"
+require_relative "thread/bbthread"
 require 'fileutils'
 require 'net/http'
 require 'uri'
