fc152fb198de34b7c6ca04f2e70d9e7e5e6a9c8a
Brandon <d2sm10@hotmail.com>
2016-02-03 18:22:36 -0700

    Added some tests for hash path.

--- spec/bblib_spec.rb
+++ spec/bblib_spec.rb
@@ -1,11 +1,19 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
-describe Bblib do
+describe BBLib do
   it 'has a version number' do
-    expect(Bblib::VERSION).not_to be nil
+    expect(BBLib::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  # Hash Path
+
+  thash = {a:1, b:2, c:{d:[3,4,5,{e:6}], f:7},g:8, 'test' => {'path' => 'here'}, e:5 }
+
+  it 'navigates hash' do
+    expect(thash.hash_path('a')).to eq [1]
+    expect(thash.hash_path('..e')).to eq [6,5]
+    expect(thash.hash_path('c.d[3].e')).to eq [6]
+    expect(thash.hash_path('test.path')).to eq ['here']
   end
+
 end
