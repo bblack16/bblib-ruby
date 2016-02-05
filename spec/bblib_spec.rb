c705370d2d432cb8bb9298527f8cf6a53edf8930
Brandon <d2sm10@hotmail.com>
2016-02-04 21:35:01 -0700

    Added more tests for hash path.

--- spec/bblib_spec.rb
+++ spec/bblib_spec.rb
@@ -16,4 +16,22 @@ describe BBLib do
     expect(thash.hash_path('test.path')).to eq ['here']
   end
 
+  it 'squishes hash' do
+    expect(thash.squish).to eq ({"a"=>1, "b"=>2, "c.d[0]"=>3, "c.d[1]"=>4, "c.d[2]"=>5, "c.d[3].e"=>6, "c.f"=>7, "g"=>8, "test.path"=>"here", "e"=>5})
+  end
+
+  squished = thash.squish
+
+  it 'expands hash' do
+    expect(squished.expand).to eq thash.keys_to_sym
+  end
+
+  it 'converts keys to strings' do
+    expect(thash.keys_to_s).to eq ({"a"=>1, "b"=>2, "c"=>{"d"=>[3, 4, 5, {"e"=>6}], "f"=>7}, "g"=>8, "test"=>{"path"=>"here"}, "e"=>5})
+  end
+
+  it 'converts keys to symbols' do
+    expect(thash.keys_to_sym).to eq ({:a=>1, :b=>2, :c=>{:d=>[3, 4, 5, {:e=>6}], :f=>7}, :g=>8, :test=>{:path=>"here"}, :e=>5})
+  end
+
 end
