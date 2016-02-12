569074452d94c3673e8ee153a149284a56acff8c
Brandon <d2sm10@hotmail.com>
2016-02-11 22:01:50 -0700

    Relocated keys_to_sym and keys_to_s to array module. Added splat operator to msplit arguments.

--- lib/array/bbarray.rb
+++ lib/array/bbarray.rb
@@ -1,7 +1,15 @@
 
 
 class Array
-  def msplit delims, keep_empty: false
+  def msplit *delims, keep_empty: false
     self.map{ |i| i.msplit(delims, keep_empty:keep_empty)}.flatten
+  end
+
+  def keys_to_sym clean: false
+    self.map{ |v| Hash === v || Array === v ? v.keys_to_sym(clean:clean) : v }
+  end
+
+  def keys_to_s clean: false
+    self.map{ |v| Hash === v || Array === v ? v.keys_to_s : v }
   end
 end
