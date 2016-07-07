1e7d9eb56f12f1d8e2f3f60700c8b5c1d2ea2572
Brandon Black <d2sm10@hotmail.com>
2016-07-07 10:29:10 -0600

    Added os_info for windows and linux.

--- lib/os/bbos.rb
+++ lib/os/bbos.rb
@@ -28,6 +28,41 @@ module BBLib
       builds = ['darwin']
       !(/#{builds.join('|')}/i =~ RUBY_PLATFORM).nil?
     end
+    
+    def self.os_info
+      if windows?
+        data = `wmic os get manufacturer,name,organization,osarchitecture,version /format:list`
+        data = data.split("\n").reject{ |r| r.strip == '' }.map do |m| 
+          spl = m.split('=')
+          [spl.first.to_clean_sym.downcase, spl[1..-1].join('=')]
+        end.to_h
+        data[:name] = data[:name].split(' |').first
+        data[:osarchitecture] = data[:osarchitecture].extract_integers.first
+        data.hpath_move( 'osarchitecture' => 'bits' )
+        data[:host] = `hostname`.strip
+        data[:os] = os
+        data
+      else
+        release = {}
+        begin
+          release = `lsb_release -a`.split("\n").map do |l|
+            spl = l.split(':')
+            [
+              spl.first.downcase.to_clean_sym,
+              spl[1..-1].join(':').strip
+            ]
+          end.to_h
+          release.hpath_move('description' => 'name', 'release' => 'name', 'distributor_id' => 'manufacturer')
+        rescue
+        end
+        {
+          release: `uname -r`,
+          bits: `uname -r` =~ /x86_64/i ? 64 : 32,
+          host: `uname -n`,
+          os: os
+        }.merge(release)
+      end
+    end
 
   end
 
