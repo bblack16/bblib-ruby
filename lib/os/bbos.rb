03e4007e83a31a7d72c5a11cef99661a897e434c
Brandon <d2sm10@hotmail.com>
2016-07-06 23:35:25 -0600

    Moved methods into new OS module and changed code to reflect this.

--- lib/os/bbos.rb
+++ lib/os/bbos.rb
@@ -3,29 +3,32 @@ require_relative 'bbsys'
 
 module BBLib
 
-  def self.os
-    return :windows if BBLib.windows?
-    return :mac if BBLib.mac?
-    return :linux if BBLib.linux?
-  end
+  module OS
 
-  def self.windows?
-    builds = ['mingw', 'mswin', 'cygwin', 'bccwin']
-    !(/#{builds.join('|')}/i =~ RUBY_PLATFORM).nil?
-  end
+    def self.os
+      return :windows if windows?
+      return :mac if mac?
+      return :linux if linux?
+    end
 
-  def self.linux?
-    !BBLib.windows? && !BBLib.mac?
-  end
+    def self.windows?
+      builds = ['mingw', 'mswin', 'cygwin', 'bccwin']
+      !(/#{builds.join('|')}/i =~ RUBY_PLATFORM).nil?
+    end
 
-  def self.unix?
-    !BBLib.windows?
-  end
+    def self.linux?
+      !windows? && !mac?
+    end
 
-  def self.mac?
-    builds = ['darwin']
-    !(/#{builds.join('|')}/i =~ RUBY_PLATFORM).nil?
-  end
+    def self.unix?
+      !windows?
+    end
 
+    def self.mac?
+      builds = ['darwin']
+      !(/#{builds.join('|')}/i =~ RUBY_PLATFORM).nil?
+    end
+
+  end
 
 end
