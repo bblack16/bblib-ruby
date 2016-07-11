7db749eb61c3a28dfac3b4d7641b315d45e62335
Brandon <d2sm10@hotmail.com>
2016-07-10 22:51:45 -0600

    Added gem specific methods.

--- lib/bblib.rb
+++ lib/bblib.rb
@@ -1,13 +1,14 @@
-require_relative "bblib/version"
-require_relative "string/bbstring"
-require_relative "file/bbfile"
-require_relative "os/bbos"
-require_relative "time/bbtime"
-require_relative "hash/bbhash"
-require_relative "number/bbnumber"
-require_relative "object/bbobject"
-require_relative "object/lazy_class"
-require_relative "array/bbarray"
+require_relative 'bblib/version'
+require_relative 'string/bbstring'
+require_relative 'file/bbfile'
+require_relative 'os/bbos'
+require_relative 'time/bbtime'
+require_relative 'hash/bbhash'
+require_relative 'gem/bbgem'
+require_relative 'number/bbnumber'
+require_relative 'object/bbobject'
+require_relative 'object/lazy_class'
+require_relative 'array/bbarray'
 require 'fileutils'
 require 'uri'
 
