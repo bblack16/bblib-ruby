8b0d65b6d6a5c5526a1086ba39defaadec2a6900
Brandon <d2sm10@hotmail.com>
2016-09-18 10:48:11 -0600

    Cleaned up code.

--- lib/opal/bbopal.rb
+++ lib/opal/bbopal.rb
@@ -5,3 +5,12 @@ module BBLib
   end
 
 end
+
+if BBLib.in_opal?
+  class Element
+
+    alias_native :replace_with, :replaceWith
+    alias_native :prepend
+    alias_native :get_context, :getContext
+  end
+end
