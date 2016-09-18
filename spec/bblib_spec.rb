f7e7338ac4ad1d30d1161977868c2316ada27c99
Brandon <d2sm10@hotmail.com>
2016-09-18 10:50:02 -0600

    Added more tests.

--- spec/bblib_spec.rb
+++ spec/bblib_spec.rb
@@ -8,8 +8,18 @@ describe BBLib do
 
   # String
 
+  it 'makes a clean sym' do
+    expect('test   one'.to_clean_sym).to eq :test_one
+    expect('TTesST!!& tw$%o'.to_clean_sym).to eq :TTesST_tw_o
+    expect(:already_a_sym.to_clean_sym).to eq :already_a_sym
+    expect("Test This".to_sym.to_clean_sym).to eq :Test_This
+  end
+
   it 'drops symbols from string' do
     expect('t.e,{s(t_)}'.drop_symbols).to eq 'test'
+    t = 't!e@s#%t'
+    t.drop_symbols!
+    expect(t).to eq 'test'
   end
 
   it 'extracts numbers from string' do
@@ -23,6 +33,10 @@ describe BBLib do
     expect('End, the'.move_articles).to eq "The End"
     expect('the end'.move_articles :back).to eq "end, The"
     expect('the best'.move_articles :none).to eq "best"
+    expect('best, an'.move_articles :none).to eq "best"
+    t = 'the test'
+    t.move_articles!(:back)
+    expect(t).to eq 'test, The'
   end
 
   it 'multi splits a string and array' do
@@ -41,6 +55,44 @@ describe BBLib do
     expect(sent.train_case).to eq 'This-Is-A-Casing-Test-Ok'
     expect(sent.camel_case).to eq 'thisIsACasingTestOk'
     expect(sent.camel_case(:upper)).to eq 'ThisIsACasingTestOk'
+    expect(sent.delimited_case('=')).to eq 'This=is=a=casing=test=OK'
+  end
+
+  it 'verifies if a string is encapsulated by a char or string' do
+    expect("(test)".encap_by?("(")).to eq true
+    expect("<test>".encap_by?("<")).to eq true
+    expect("[test]".encap_by?("[")).to eq true
+    expect("{test}".encap_by?("{")).to eq true
+    expect("1test1".encap_by?("1")).to eq true
+    expect("testtesttest".encap_by?("test")).to eq true
+    expect("hello test hello".encap_by?("hello")).to eq true
+    expect("(test".encap_by?("(")).to eq false
+    expect("[test".encap_by?("[")).to eq false
+  end
+
+  it 'removes encapsulated characters or strings from a string' do
+    expect("(test)".uncapsulate("(")).to eq 'test'
+    expect("<test>".uncapsulate("<")).to eq 'test'
+    expect("[test]".uncapsulate("[")).to eq 'test'
+    expect("{test}".uncapsulate("{")).to eq 'test'
+    expect("1test1".uncapsulate("1")).to eq 'test'
+    expect("testtesttest".uncapsulate("test")).to eq 'test'
+    expect("hello test hello".uncapsulate("hello")).to eq ' test '
+    expect("(test".uncapsulate("(")).to eq 'test'
+    expect("[test".uncapsulate("[")).to eq 'test'
+  end
+
+  it 'converts roman numerals' do
+    expect(2.to_roman).to eq 'II'
+    expect("Toy story 3".to_roman).to eq 'Toy story III'
+    expect('Left IV Dead'.from_roman).to eq 'Left 4 Dead'
+    expect('lIVe IIn fear'.to_roman).to eq 'lIVe IIn fear'
+    t = 'Donkey Kong Country 3'
+    t.to_roman!
+    expect(t).to eq 'Donkey Kong Country III'
+    t = 'Title VII'
+    t.from_roman!
+    expect(t).to eq 'Title 7'
   end
 
   # Number
@@ -58,7 +110,10 @@ describe BBLib do
   # Hash
 
   it 'reverses hash keys' do
-    expect(({a:1,b:2,c:4,d:{e:5}}).reverse).to eq ({:d=>{:e=>5}, :c=>4, :b=>2, :a=>1})
+    hash = {a:1,b:2,c:4,d:{e:5}}
+    expect(hash.reverse).to eq ({:d=>{:e=>5}, :c=>4, :b=>2, :a=>1})
+    hash.reverse!
+    expect(hash).to eq ({:d=>{:e=>5}, :c=>4, :b=>2, :a=>1})
   end
 
   it 'deep merges a hash' do
@@ -67,6 +122,14 @@ describe BBLib do
     expect(a.deep_merge b).to eq ({:a=>1, :b=>[2, 3, 8], :c=>{:d=>{:e=>9, :f=>"test", :g=>0}}})
     expect(a.deep_merge b, merge_arrays: false).to eq ({:a=>1, :b=>[8], :c=>{:d=>{:e=>9, :f=>"test", :g=>0}}})
     expect(a.deep_merge b, overwrite: false).to eq ({:a=>1, :b=>[2, 3, 8], :c=>{:d=>{:e=>[4, 9], :f=>[5, 6, 7, "test"], :g=>0}}})
+    a.deep_merge! b
+    expect(a).to eq ({:a=>1, :b=>[2, 3, 8], :c=>{:d=>{:e=>9, :f=>"test", :g=>0}}})
+  end
+
+  it 'places a key at the beginning of a hash (unshift)' do
+    a = {b: 2}
+    a.unshift(a: 1)
+    expect(a).to eq ({a:1, b:2})
   end
 
   # Hash Path
@@ -102,10 +165,16 @@ describe BBLib do
 
   it 'converts keys to strings' do
     expect(thash.keys_to_s).to eq ({"a"=>1, "b"=>2, "c"=>{"d"=>[3, 4, 5, {"e"=>6}], "f"=>7}, "g"=>8, "test"=>{"path"=>"here"}, "e"=>5})
+    bhash = thash.clone
+    bhash.keys_to_s!
+    expect(bhash).to eq ({"a"=>1, "b"=>2, "c"=>{"d"=>[3, 4, 5, {"e"=>6}], "f"=>7}, "g"=>8, "test"=>{"path"=>"here"}, "e"=>5})
   end
 
   it 'converts keys to symbols' do
     expect(thash.keys_to_sym).to eq ({:a=>1, :b=>2, :c=>{:d=>[3, 4, 5, {:e=>6}], :f=>7}, :g=>8, :test=>{:path=>"here"}, :e=>5})
+    bhash = thash.clone
+    bhash.keys_to_sym!
+    expect(bhash).to eq ({:a=>1, :b=>2, :c=>{:d=>[3, 4, 5, {:e=>6}], :f=>7}, :g=>8, :test=>{:path=>"here"}, :e=>5})
   end
 
   # Hash Path Proc
@@ -128,6 +197,11 @@ describe BBLib do
   end
 
   # Time
+
+  it 'converts integers to duration strings' do
+    expect(166.to_duration).to eq '2 mins 46 secs'
+    expect(166.to_duration(input: :min)).to eq '2 hrs 46 mins'
+  end
 
   it 'parses time from string' do
     expect('1min'.parse_duration output: :sec).to eq 60.0
@@ -163,6 +237,38 @@ describe BBLib do
     expect('5000123 mil'.parse_duration output: :hour).to eq 1.3889230555555556
     expect('55hrs'.parse_duration output: :hour).to eq 55.0
     expect('1year 1month 1day 1hour 1 min 1 sec 1 mil'.parse_duration output: :hour).to eq 9505.016944722222
+  end
+
+  # Array
+
+  it 'interleaves array' do
+    a, b = [1,3,5], [2, 4, 6]
+    expect(a.interleave(b)).to eq [1, 2, 3, 4, 5, 6]
+    a, b = [1,3,5], [2, 4, 6, 7, 8, 9]
+    expect(a.interleave(b)).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  it 'multi splits arrays and strings' do
+    expect('1&2||3,4TEST56666'.msplit('&', '||', ',', /6{3}/, /test/i)).to eq ['1','2','3','4','5','6']
+    a = ['1&2||', '3,4', '56666']
+    expect(a.msplit('&', '||', ',', /6{3}/, /test/i)).to eq ['1','2','3','4','5','6']
+  end
+
+  it 'gets diff of arrays' do
+    expect([1,2,3,5].diff([1,2,4])).to eq [3,5,4]
+    expect([1,2,3,5,'test'].diff([1,2,4,nil, :test])).to eq [3, 5, "test", 4, nil, :test]
+  end
+
+  it 'converts a hash to xml' do
+    expect(({test: [1,2], name: 'John', data: { id: 4, title: 'Something'}}).to_xml).to eq "<test>\n\t1\n</test>\n<test>\n\t2\n</test>\n<name>\n\tJohn\n</name>\n<data>\n\t<id>\n\t\t4\n\t</id>\n\t<title>\n\t\tSomething\n\t</title>\n</data>"
+  end
+
+  # Cron
+
+  # TODO - Better Cron testing
+  it 'parses cron syntax' do
+    expect(BBLib::Cron.next('*/5 * * * * *') > Time.now).to eq true
+    expect(BBLib::Cron.prev('*/5 * * * * *') < Time.now).to eq true
   end
 
 end
