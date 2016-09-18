50ec46e7afeba401a3b25997fc2c6eaf8f460d51
Brandon <d2sm10@hotmail.com>
2016-09-18 10:47:50 -0600

    Added serialization methods to LazyClass.

--- lib/object/lazy_class.rb
+++ lib/object/lazy_class.rb
@@ -4,10 +4,23 @@ module BBLib
   class LazyClass
     extend Hooks
     extend Attr
+    attr_reader :_serialize_fields
 
     def initialize *args
+      _pre_setup
       lazy_setup
-      lazy_init(*args)
+      _lazy_init(*args)
+    end
+
+    def serialize
+      _serialize_fields.map do |name, h|
+        value = send(h[:method])
+        if !h[:always] && value == h[:ignore]
+          nil
+        else
+          [ name, value ]
+        end
+      end.reject(&:nil?).to_h
     end
 
     protected
@@ -26,12 +39,33 @@ module BBLib
         custom_lazy_init BBLib::named_args(*args), *args
       end
 
+      def _pre_setup
+        self.methods.each do |m|
+          if m.to_s.start_with?('__reset_')
+            send(m) rescue nil
+          end
+        end
+      end
+
       def lazy_init *args
         # Define custom initialization here...
       end
 
       def custom_lazy_init *args
         # Left in for legacy support...don't use this!
+      end
+
+      def serialize_method name, method = nil, ignore: nil, always: false
+        return if method == :serialize || name == :serialize && method.nil?
+        _serialize_fields[name.to_sym] = {
+          method: (method.nil? ? name.to_sym : method.to_sym),
+          ignore: ignore,
+          always: always
+        }
+      end
+
+      def _serialize_fields
+        @_serialize_fields ||= Hash.new
       end
 
   end
