0cc042242a7a633bbaf0facb1359caf32fafb280
Brandon <d2sm10@hotmail.com>
2016-11-02 22:44:52 -0600

    Added loop_between method.

--- lib/number/bbnumber.rb
+++ lib/number/bbnumber.rb
@@ -3,9 +3,16 @@ module BBLib
   # Passing nil as min or max represents no bounds in that direction.
   # min and max are inclusive to the allowed bounds.
   def self.keep_between(num, min, max)
-    raise "Argument must be numeric: #{num} (#{num.class})" unless num.is_a?(Numeric)
+    raise ArgumentError, "Argument must be numeric: #{num} (#{num.class})" unless num.is_a?(Numeric)
     num = min if min && num < min
     num = max if max && num > max
+    num
+  end
+
+  def self.loop_between(num, min, max)
+    raise ArgumentError, "Argument must be numeric: #{num} (#{num.class})" unless num.is_a?(Numeric)
+    num = max if min && num < min
+    num = min if max && num > max
     num
   end
 end
