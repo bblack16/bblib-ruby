32622bf71ce664286dab259f0b0c3a6818add16c
Brandon <d2sm10@hotmail.com>
2016-11-21 23:48:16 -0700

    Added conversion to float in the event the num param is nil or a string.

--- lib/number/bbnumber.rb
+++ lib/number/bbnumber.rb
@@ -3,14 +3,14 @@ module BBLib
   # Passing nil as min or max represents no bounds in that direction.
   # min and max are inclusive to the allowed bounds.
   def self.keep_between(num, min, max)
-    raise ArgumentError, "Argument must be numeric: #{num} (#{num.class})" unless num.is_a?(Numeric)
+    num = num.to_f unless num.is_a?(Numeric)
     num = min if min && num < min
     num = max if max && num > max
     num
   end
 
   def self.loop_between(num, min, max)
-    raise ArgumentError, "Argument must be numeric: #{num} (#{num.class})" unless num.is_a?(Numeric)
+    num = num.to_f unless num.is_a?(Numeric)
     num = max if min && num < min
     num = min if max && num > max
     num
