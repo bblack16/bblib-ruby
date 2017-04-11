b2c687319f44c82d101a269a9cbcba6529dd502f
Brandon <d2sm10@hotmail.com>
2017-04-10 23:15:48 -0600

    Added documentation.

--- lib/object/lazy_class.rb
+++ lib/object/lazy_class.rb
@@ -1,6 +1,12 @@
 
 # frozen_string_literal: true
 module BBLib
+  # An advanced class that supports hooking methods, more advanced attr_ getter/setter
+  # methods and other useful methods to mkae classes easier to work with.
+  #   Note: This class should be treated as abstract. It is useless on its own and should
+  #   be inherited from.
+  #
+  # @author Brandon Black
   class LazyClass
     extend Hooks
     extend Attr
@@ -15,6 +21,8 @@ module BBLib
       _validate_required_params
     end
 
+    # Serializes all attr methods that have been set to serialize: true.
+    #   This serialization is recursive to other LazyClass objects in any attributes on this class.
     def serialize
       _serialize_fields.map do |name, h|
         next if _dont_serialize_fields.include?(name)
@@ -55,6 +63,7 @@ module BBLib
       @_dont_serialize_fields ||= []
     end
 
+    # Returns a hash of all the attr methods on this object and their options.
     def attrs
       self.class.attrs
     end
@@ -73,16 +82,19 @@ module BBLib
       end
     end
 
+    # Lists all attr_* getter methods that were created on this class.
     def self.instance_getters
       attrs.map { |k, v| [:attr_writer].any? { |t| v[:type] == t } ? nil : k }.compact
     end
 
+    # Lists all attr_* setter methods that were created on this class.
     def self.instance_setters
       attrs.keys.map { |m| "#{m}=".to_sym }.select { |m| method_defined?(m) }
     end
 
     protected
 
+    # Hook for subclasses to use prior to argument handling of initialize.
     def lazy_setup
       # Instantiate necessary variables here
     end
@@ -124,14 +136,24 @@ module BBLib
       end
     end
 
+    # Hook for subclasses to process arguments passed in to initialize.
+    # This is called after _lazy_init which should NOT be overriden.
     def lazy_init(*args)
       # Define custom initialization here...
     end
 
+    # @deprecated Use {#lazy_init} instead.
     def custom_lazy_init(*args)
       # Left in for legacy support...don't use this!
     end
 
+    # Used to setup a method for serialization by means of .serialize
+    #
+    # @param [Symbol] name the name of the key to use when this object is serialized.
+    # @param [Symbol] method the method to call to serialize. If this is not provided, name is also used as the method name.
+    # @param [Object] ignore if the value the method returns == this value, it will not be included in the serialized hash.
+    # @param [TrueClass, FalseClass] always If set to true, this method will always be included in the serialized hash, even if it matches ignore.
+    # @return [Hash] Returns the opts hash that will be saved for this method.
     def serialize_method(name, method = nil, ignore: nil, always: false)
       return if method == :serialize || name == :serialize && method.nil?
       _serialize_fields[name.to_sym] = {
@@ -141,6 +163,9 @@ module BBLib
       }
     end
 
+    # Used to prevent methods from being serialized.
+    # This is useful when inheriting from a LazyClass subclass that has a method
+    # set up for serialization that should not be included in this subclass.
     def dont_serialize_method(*names)
       names.each { |name| _dont_serialize_fields.push(name) unless _dont_serialize_fields.include?(name) }
     end
