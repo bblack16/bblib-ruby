82eb8a5ccbab5874a10e288c8158699a629b3499
Brandon Black <d2sm10@hotmail.com>
2017-07-24 07:48:18 -0600

    Fixed ancestor lookup and added some guards around infinitely recursive serializations.

--- lib/mixins/serializer.rb
+++ lib/mixins/serializer.rb
@@ -14,8 +14,8 @@ module BBLib
 
       def _ancestor_serialize_fields
         hash = {}
-        self.class.ancestors.reverse.map do |ancestor|
-          next unless ancestor.respond_to?(:_serialize_fields)
+        ancestors.reverse.map do |ancestor|
+          next unless ancestor.respond_to?(:_serialize_fields) && ancestor != self
           hash = hash.deep_merge(ancestor._serialize_fields)
         end
         hash
@@ -26,10 +26,10 @@ module BBLib
       end
 
       def _ancestor_dont_serialize_fields
-        self.class.ancestors.reverse.flat_map do |ancestor|
-          next unless ancestor.respond_to?(:_dont_serialize_fields)
-          hash = hash.deep_merge(ancestor._dont_serialize_fields)
-        end.uniq
+        ancestors.reverse.flat_map do |ancestor|
+          next unless ancestor.respond_to?(:_dont_serialize_fields) && ancestor != self
+          ancestor._dont_serialize_fields
+        end.compact.uniq
       end
 
       def dont_serialize_method(method)
@@ -44,21 +44,22 @@ module BBLib
       end
     end
 
-    def serialize
+    def serialize(ignore_defaults = false)
       self.class._serialize_fields.map do |name, opts|
         next if self.class._dont_serialize_fields.include?(name)
         args = [opts[:method]] + (opts.include?(:args) ? [opts[:args]].flatten(1) : [])
         value = send(*args)
+        next if value == self
         unless opts[:flat]
           if value.is_a?(Hash)
-            value = value.map { |k, v| [k, v.respond_to?(:serialize) ? v.serialize : v] }.to_h
+            value = value.map { |k, v| [k, v != self && v.respond_to?(:serialize) ? v.serialize(ignore_defaults) : v] }.to_h
           elsif value.is_a?(Array)
-            value = value.map { |v| v.respond_to?(:serialize) ? v.serialize : v }
+            value = value.map { |v| v != self && v.respond_to?(:serialize) ? v.serialize(ignore_defaults) : v }
           elsif value.respond_to?(:serialize)
-            value = value.serialize
+            value = value.serialize(ignore_defaults)
           end
         end
-        if !opts[:always] && (opts.include?(:ignore) && value == opts[:ignore])
+        if !opts[:always] && (ignore_defaults && value == opts[:default] || opts.include?(:ignore) && value == opts[:ignore])
           nil
         else
           [name, value]
