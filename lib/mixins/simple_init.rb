5e69d3e557a1d3de8dca7a5b2c0c364d246f3a83
Brandon Black <d2sm10@hotmail.com>
2017-07-24 07:48:57 -0600

    Added some minor fixes and added support for cascading init type.

--- lib/mixins/simple_init.rb
+++ lib/mixins/simple_init.rb
@@ -13,18 +13,27 @@ module BBLib
       base.class_eval do
         define_method(:initialize) do |*args, &block|
           send(:simple_setup) if respond_to?(:simple_setup, true)
+          send(:simple_preinit, *args, &block) if respond_to?(:simple_preinit, true)
           _initialize(*args)
           send(:simple_init, *args, &block) if respond_to?(:simple_init, true)
-          yield self if block_given?
+          block.call(self) if block
         end
       end
     end
 
     module ClassMethods
       def init_type(type = nil)
-        return @init_type ||= :strict unless type
+        return @init_type ||= _super_init_type unless type
         raise ArgumentError, "Unknown init type '#{type}'. Must be #{INIT_TYPES.join_terms('or', encapsulate: "'")}." unless INIT_TYPES.include?(type)
         @init_type = type
+      end
+
+      def _super_init_type
+        ancestors.each do |ancestor|
+          next if ancestor == self
+          return ancestor.init_type if ancestor.respond_to?(:init_type)
+        end
+        :strict
       end
     end
 
@@ -35,6 +44,7 @@ module BBLib
       named = BBLib.named_args(*args)
       if self.class.respond_to?(:_attrs)
         missing = self.class._attrs.map do |method, details|
+          next if send(method)
           details[:options][:required] && !named.include?(method) ? method : nil
         end.compact
         raise ArgumentError, "You are missing the following required #{BBLib.pluralize(missing.size, 'argument')}: #{missing.join_terms}" unless missing.empty?
