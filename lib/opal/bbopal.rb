a8b0e1a427315a2955e03608f80652f96eb8c059
Brandon <d2sm10@hotmail.com>
2017-11-10 07:52:39 -0700

    Added new error to be raised when an Opal specific method is called outside of Opal.

--- lib/opal/bbopal.rb
+++ lib/opal/bbopal.rb
@@ -2,6 +2,9 @@ module BBLib
   def self.in_opal?
     RUBY_ENGINE == 'opal'
   end
+
+  # Used when a method is called while in the wrong engine (such as Opal)
+  class WrongEngineError < StandardError; end
 end
 
 if BBLib.in_opal?
