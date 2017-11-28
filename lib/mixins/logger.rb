141288040cc819d2e7012026c95dcdf9fcdb0d85
Brandon Black <d2sm10@hotmail.com>
2017-11-28 08:06:57 -0700

    Temporarily commented out logger methods. Need to revisit this class.

--- lib/mixins/logger.rb
+++ lib/mixins/logger.rb
@@ -5,11 +5,11 @@ module BBLib
       self.class.logger
     end
 
-    [:debug, :info, :warn, :error, :fatal, :unknown].each do |sev|
-      define_method(sev) do |msg = nil, &block|
-          logger.send(sev) { "[#{self.class}] #{msg ? msg : block.call}" }
-      end
-    end
+    # [:debug, :info, :warn, :error, :fatal, :unknown].each do |sev|
+    #   define_method(sev) do |msg = nil, &block|
+    #       logger.send(sev) { "[#{self.class}] #{msg ? msg : block.call}" }
+    #   end
+    # end
 
     def self.included(base)
       base.extend ClassMethods
@@ -20,11 +20,11 @@ module BBLib
         BBLib.logger
       end
 
-      [:debug, :info, :warn, :error, :fatal, :unknown].each do |sev|
-        define_method(sev) do |msg = nil, &block|
-            logger.send(sev) { "[#{self}] #{msg ? msg : block.call}" }
-        end
-      end
+      # [:debug, :info, :warn, :error, :fatal, :unknown].each do |sev|
+      #   define_method(sev) do |msg = nil, &block|
+      #       logger.send(sev) { "[#{self}] #{msg ? msg : block.call}" }
+      #   end
+      # end
     end
 
   end
