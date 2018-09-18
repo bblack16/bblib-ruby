49cdf88e1f4e3795c7280ae897d9f5d14c750bc9
Brandon Black <d2sm10@hotmail.com>
2018-09-18 07:12:31 -0600

    Aliased to :at

--- lib/bblib/cli/options/command.rb
+++ lib/bblib/cli/options/command.rb
@@ -1,9 +1,14 @@
 module BBLib
   class OptsParser
     class Command < Option
+      attr_of [Integer, Range], :position, default: nil, allow_nil: true, arg_at: 0
+
+      def self.type
+        [super, :at]
+      end
 
       def extract(index, args)
-        args.delete_at(index)
+        args.delete_at(index).to_s
       end
 
     end
