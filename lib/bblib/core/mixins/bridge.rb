bb5a3d2dec33c1d697b7d35f0560dc332a5df1a3
Brandon Black <d2sm10@hotmail.com>
2019-02-21 23:33:18 -0700

    Added missing pass of block arguments to bridged methods.

--- lib/bblib/core/mixins/bridge.rb
+++ lib/bblib/core/mixins/bridge.rb
@@ -6,8 +6,8 @@ module BBLib
 
     def bridge_method(*class_methods)
       class_methods.each do |class_method|
-        define_method(class_method) do |*args|
-          self.class.send(class_method, *args)
+        define_method(class_method) do |*args, &block|
+          self.class.send(class_method, *args, &block)
         end
       end
       true
